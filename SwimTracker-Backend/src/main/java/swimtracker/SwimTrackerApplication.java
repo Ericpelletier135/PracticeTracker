6d0c76d4e002409a7c16f75668d8740088ee0234
Eric <ericpelletier135@gmail.com>
2022-09-18 21:09:22 -0400

    initial backend commit

---
+++ SwimTracker-Backend/src/main/java/swimtracker/SwimTrackerApplication.java
@@ -0,0 +1,21 @@
+package swimtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class SwimTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimTrackerApplication.class, args);
+	}
+
+	@RequestMapping("/")
+  	public String greeting(){
+    		return "Hello world!";
+  	}
+
+}
---
+++ SwimTracker-Backend/src/test/java/swimtracker/SwimTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package swimtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
